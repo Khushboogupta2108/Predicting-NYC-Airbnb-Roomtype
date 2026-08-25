9061a17787674bd018917d2e94835451db96ac64
Khushboo Gupta <realkhushboogupta@gmail.com>
2026-08-25 18:40:42 +0530

    Connect frontend to deployed API

--- frontend/script.js
+++ frontend/script.js
@@ -30,7 +30,7 @@ form.addEventListener("submit", async (event) => {
     };
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://predicting-nyc-airbnb-roomtype-1.onrender.com/predict", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
